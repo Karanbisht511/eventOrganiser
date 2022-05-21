66c73a29cfa387595104c92f7dc7dc4004f0da9a
Karan Bisht <karanbisht511@gmail.com>
2022-05-21 11:27:49 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -12,18 +12,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
-// const url =
-//   "mongodb+srv://karan:kgjxaAVKsH6HSBXf@cluster0.kw70c.mongodb.net/test";
 
-// mongoose
-//   .connect(url, { useUnifiedTopology: true })
-//   .then(() => {
-//     console.log("Database is connected");
-//   })
-//   .catch((error) => {
-//     console.log("Error is occured", error);
-//   });
-// const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use("/", indexRouter);
